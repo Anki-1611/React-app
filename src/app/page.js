b69d032c304d1321abb5a1e750af699130e656a9
Anki-1611 <ankappa.As@nttdata.com>
2025-03-24 17:19:08 +0530

    erwqrwqr
    rwqr

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ const Navbar = () => {
         <li><a href="#about" className="text-white">About</a></li>
         <li><a href="#contact" className="text-white">Contact</a></li>
         <li><a href="#contact" className="text-white">Blog</a></li>
-        <li><a href="#contact" className="text-white">features</a></li>
 
 
       </ul>
