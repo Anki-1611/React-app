6b4ff74c92b6a203709f554a16886e6d9d444c27
Anki-1611 <ankappa.As@nttdata.com>
2025-03-24 16:03:44 +0530

    changed file 1
    changed file 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,12 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+
+const geistSans1= Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
