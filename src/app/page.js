2e6c27ac1249d7cd8bb3effaafe69c10c99804e9
Ankappa <ankappa.As@nttdata.com>
2025-07-30 15:18:13 +0530

    duplicate one
    duplicate one

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,10 @@ const Navbar = () => {
         <li><a href="#home" className="text-white">Home</a></li>
         <li><a href="#about" className="text-white">About</a></li>
         <li><a href="#contact" className="text-white">Contact</a></li>
-        <li><a href="#contact" className="text-white">Blog dev 22nd test </a></li>
+        <li><a href="#contact" className="text-white">Blog dev 21nd test </a></li>
+         <li><a href="#contact" className="text-white">Blog dev 22nd test </a></li>
+          <li><a href="#contact" className="text-white">Blog dev 23nd test </a></li>
+           <li><a href="#contact" className="text-white">Blog dev 24nd test </a></li>
       </ul>
     </nav>
   );
